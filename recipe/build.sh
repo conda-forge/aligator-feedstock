d40179bca11a51843f4ea2b6fde6a408fe2403f9
Joris Vaillant <joris.vaillant@inria.fr>
2024-05-31 10:51:43 +0200

    Add ppc and arm architecture

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,26 @@ rm -rf build
 mkdir build
 cd build
 
+export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+
+echo $BUILD_NUMPY_INCLUDE_DIRS
+echo $TARGET_NUMPY_INCLUDE_DIRS
+
+export GENERATE_PYTHON_STUBS=1
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
+  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
+  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
+  export GENERATE_PYTHON_STUBS=0
+fi
+
 cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DPYTHON_EXECUTABLE=$PYTHON \
       -DBUILD_PYTHON_INTERFACE=ON \
-      -DGENERATE_PYTHON_STUBS=ON \
+      -DGENERATE_PYTHON_STUBS=$GENERATE_PYTHON_STUBS \
+      -DPython3_NumPy_INCLUDE_DIR=$TARGET_NUMPY_INCLUDE_DIRS \
       -DBUILD_WITH_OPENMP_SUPPORT=ON \
       -DBUILD_CROCODDYL_COMPAT=ON \
       -DBUILD_EXAMPLES=ON \
@@ -21,3 +36,7 @@ cmake --build . --parallel ${CPU_COUNT}
 
 # install
 cmake --build . --parallel ${CPU_COUNT} --target install
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  sed -i 's|'"$BUILD_PREFIX"'|'"$PREFIX"'|g' $PREFIX/lib/cmake/aligator/aligatorTargets.cmake
+fi

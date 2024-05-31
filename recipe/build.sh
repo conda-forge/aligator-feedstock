75207b93cf02e4942b82379952a55ddbb4ec65ed
Joris Vaillant <joris.vaillant@inria.fr>
2024-05-31 11:02:33 +0200

    Deactivate crocoddyl on aarch64 and ppc64le

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,18 +5,26 @@ rm -rf build
 mkdir build
 cd build
 
-export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
-export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
 
 echo $BUILD_NUMPY_INCLUDE_DIRS
 echo $TARGET_NUMPY_INCLUDE_DIRS
 
-export GENERATE_PYTHON_STUBS=1
+GENERATE_PYTHON_STUBS=1
 if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
   echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
   mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
   cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
-  export GENERATE_PYTHON_STUBS=0
+  GENERATE_PYTHON_STUBS=0
+fi
+
+# crocoddyl package doesn't exists on linux_aarch64 and linux_ppc64le architecture
+BUILD_CROCODDYL_COMPAT=1
+if [[ $HOST =~ linux ]]; then
+  if [[ $HOST =~ aarch64 || $HOST =~ powerpc64le ]]; then
+    BUILD_CROCODDYL_COMPAT=0
+  fi
 fi
 
 cmake ${CMAKE_ARGS} .. \
@@ -26,7 +34,7 @@ cmake ${CMAKE_ARGS} .. \
       -DGENERATE_PYTHON_STUBS=$GENERATE_PYTHON_STUBS \
       -DPython3_NumPy_INCLUDE_DIR=$TARGET_NUMPY_INCLUDE_DIRS \
       -DBUILD_WITH_OPENMP_SUPPORT=ON \
-      -DBUILD_CROCODDYL_COMPAT=ON \
+      -DBUILD_CROCODDYL_COMPAT=$BUILD_CROCODDYL_COMPAT \
       -DBUILD_EXAMPLES=ON \
       -DBUILD_BENCHMARK=OFF \
       -DBUILD_TESTING=OFF

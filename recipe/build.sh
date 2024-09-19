1d45ec5c4af9e643d0cf4cf85942c6b4a9db6f2c
ManifoldFR <wilson.jallet@polytechnique.org>
2024-09-19 11:14:15 +0200

    Cap no. of cpus
    spaces bad

--- recipe/build.sh
+++ recipe/build.sh
@@ -39,8 +39,12 @@ cmake ${CMAKE_ARGS} .. \
       -DBUILD_BENCHMARK=OFF \
       -DBUILD_TESTING=OFF
 
+NCPU=$((${CPU_COUNT} - 1))
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  NCPU=1
+fi
 # build
-cmake --build . --parallel ${CPU_COUNT}
+cmake --build . --parallel $NCPU
 
 # install
 cmake --build . --target install
